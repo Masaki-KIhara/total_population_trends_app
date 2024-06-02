5b5498b17a6b28da13207b3af17691286ce2aee2
Masaki Kihara <masaki1224.1998@gmail.com>
2024-06-02 21:07:41 +0900

    都道府県取得、各都道府県のデータ取得実装

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,61 @@
+import { useState } from "react";
+import { useEffect } from "react";
+
+type GetPrefectureType = { prefCode: number; prefName: string }[];
+
+type FetchPrefectureDataType<T> = {
+  message: null;
+  result: T;
+};
+
 export default function Home() {
-  return <p>aaaa</p>;
+  const [prefData, setPrefData] = useState<GetPrefectureType>([]);
+  const getPrefectureData = () => {
+    fetch("https://opendata.resas-portal.go.jp/api/v1/prefectures", {
+      headers: {
+        "x-api-key": process.env.NEXT_PUBLIC_RESAS_API_KYE as string,
+      },
+    })
+      .then((res) => res.json())
+      .then((data: FetchPrefectureDataType<GetPrefectureType>) =>
+        setPrefData(data.result)
+      )
+      .catch(() => {
+        console.log("エラー");
+      });
+  };
+
+  const getPrefData = (prefCode: number) => {
+    fetch(
+      `https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear?cityCode=11362&prefCode=${prefCode}`,
+      {
+        headers: {
+          "x-api-key": process.env.NEXT_PUBLIC_RESAS_API_KYE as string,
+        },
+      }
+    )
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  };
+
+  useEffect(() => {
+    getPrefectureData();
+  }, []);
+
+  return (
+    <>
+      {prefData.map((item) => (
+        <label key={item.prefCode}>
+          {item.prefName}
+          <input
+            type={"checkbox"}
+            value={item.prefCode}
+            onChange={() => {
+              getPrefData(item.prefCode);
+            }}
+          />
+        </label>
+      ))}
+    </>
+  );
 }
